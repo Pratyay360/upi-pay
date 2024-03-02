e0c2c2d56d4ddaa70da60bc2073667226816c185
Pratyay360 <pratyaymustafi@gmail.com>
2024-03-02 23:58:56 +0530

    minor updates 2

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-          {hostname: 'raw.githubusercontent.com'},
-          {hostname: 'github.com'},
-          {hostname: 'wekwttnnowtwqzntesch.supabase.co'}, 
-          {hostname: 'cdn.hashnode.com'},
-          {hostname: 'private-user-images.githubusercontent.com'},
-        ],
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { hostname: "raw.githubusercontent.com" },
+      { hostname: "github.com" },
+      { hostname: "wekwttnnowtwqzntesch.supabase.co" },
+      { hostname: "cdn.hashnode.com" },
+      { hostname: "private-user-images.githubusercontent.com" },
+    ],
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-}
+};
